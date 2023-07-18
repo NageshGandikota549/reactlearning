c3bab5021e9766e4aa9f9ce3b076c1b99fff771a
nagesh <nageshyour@gmail.com>
2023-07-18 22:59:44 +0530

    added useref

--- src/products/Products.tsx
+++ src/products/Products.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./Products.css";
 import FilterSection, { IFilter } from "./FilterSection";
 
@@ -21,16 +21,26 @@ export const Products: React.FC = () => {
   const [filter, setFilter] = useState<IFilter>(new Filter());
   const [url, setUrl] = useState("");
   const [categories, setCategories] = useState([]);
+  const [searchText, setSearchText] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     setUrl(`https://fakestoreapi.com/products/category/${filter.category}`);
   }, [filter.category]);
-
+  // Updating
   useEffect(() => {
     setUrl(
       `https://fakestoreapi.com/products?limit=${filter.perpage}&sort=${filter.sortOrder}`
     );
   }, [filter.perpage, filter.sortOrder]);
+
+  // LifeCycle methods - Component Mounting, Updating, Unmounting
+
+  //Mounting
+  useEffect(() => {
+    inputRef.current?.focus();
+    // if (inputRef.current) inputRef.current.style.backgroundColor = "red";
+  }, []);
 
   useEffect(() => {
     fetch(url)
@@ -56,11 +66,31 @@ export const Products: React.FC = () => {
     filter: filter,
   };
 
+  const filteredProducts = products.filter(
+    (x) =>
+      searchText.trim() === "" ||
+      x.title
+        .toLocaleLowerCase()
+        .trim()
+        .includes(searchText.toLocaleLowerCase().trim())
+  );
+
   return (
     <>
       <FilterSection {...filterSectionProps} />
+      <div>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Search Products.."
+          onChange={(event) => {
+            setSearchText(event.target.value);
+          }}
+          value={searchText}
+        />
+      </div>
       <div className="grid-container">
-        {products.map((product) => {
+        {filteredProducts.map((product) => {
           return (
             <div key={product.id}>
               <img
